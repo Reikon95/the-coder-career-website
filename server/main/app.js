3cfe42881828111152dd16e3240d229c055e039a
Cameron Blackwood <cameronjwblackwood@gmail.com>
2020-10-29 08:23:51 +0000

    backend setup

--- server/main/app.js
+++ server/main/app.js
@@ -1,16 +1,43 @@
-const createError = require("http-errors");
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const { ExpressOIDC } = require("@okta/oidc-middleware");
 const app = express();
+const port = 3000;
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// session support is required to use ExpressOIDC
+app.use(
+  session({
+    secret: process.env.RANDOM_SECRET_WORD,
+    resave: true,
+    saveUninitialized: false,
+  })
+);
 
+const oidc = new ExpressOIDC({
+  issuer: `${process.env.OKTA_ORG_URL}/oauth2/default`,
+  client_id: process.env.OKTA_CLIENT_ID,
+  client_secret: process.env.OKTA_CLIENT_SECRET,
+  redirect_uri: process.env.REDIRECT_URL,
+  scope: "openid profile",
+  routes: {
+    callback: {
+      path: "/authorization-code/callback",
+      defaultRedirect: "/admin",
+    },
+  },
+});
 
-module.exports = app;
+// ExpressOIDC will attach handlers for the /login and /authorization-code/callback routes
+app.use(oidc.router);
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome!!</h1>");
+});
+
+app.listen(port, () => console.log(`My Blog App listening on port ${port}!`));
